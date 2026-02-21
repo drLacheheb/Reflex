5c94a0602be3dcb3015fc9516cbfad573875f1e3
drLacheheb <yacinolach@gmail.com>
2026-02-21 15:23:01 +0100

    feat: integrate sentry for error tracking

--- apps/web/app/(dashboard)/page.tsx
+++ apps/web/app/(dashboard)/page.tsx
@@ -1,12 +1,11 @@
 "use client";
 
-import {useMutation, useQuery} from "convex/react";
+import {useMutation} from "convex/react";
 import {api} from "@workspace/backend/_generated/api";
 import {Button} from "@workspace/ui/components/button";
 import {OrganizationSwitcher, UserButton} from "@clerk/nextjs";
 
 const Page = () => {
-    const users = useQuery(api.users.getMany);
     const addUser = useMutation(api.users.add)
 
     return (
@@ -16,10 +15,11 @@ const Page = () => {
             <OrganizationSwitcher
                 hidePersonal
             />
-            <Button onClick={() => addUser()}>Add</Button>
-            <div className="max-w-sm w-full mx-auto">
-                {JSON.stringify(users, null, 2)}
-            </div>
+            <Button
+                onClick={() => addUser()}
+            >
+                Add
+            </Button>
         </div>
     );
 }
