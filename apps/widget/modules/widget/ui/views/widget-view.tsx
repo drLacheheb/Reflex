7ab8263202f24dddce63643254e1329012c833fc
drLacheheb <yacinolach@gmail.com>
2026-02-25 17:29:18 +0100

    feat: integrate jotai state management and contact session mutation

--- apps/widget/modules/widget/ui/views/widget-view.tsx
+++ apps/widget/modules/widget/ui/views/widget-view.tsx
@@ -1,9 +1,8 @@
 "use client";
 
-/*
-import {WidgetFooter} from "@/modules/widget/ui/components/widget-footer";
-*/
+import {useAtomValue} from "jotai";
 import {WidgetAuthScreen} from "@/modules/widget/ui/screens/widget-auth-screen";
+import {screenAtom} from "@/modules/widget/atoms/widget-atom";
 
 interface Props {
     organizationId: string;
@@ -12,12 +11,23 @@ interface Props {
 export const WidgetView = (
     {organizationId}: Props
 ) => {
+    const screen = useAtomValue(screenAtom);
+    const screenComponents = {
+        error: <p>TODO: Error</p>,
+        loading: <p>TODO: Loading</p>,
+        selection: <p>TODO: Selection</p>,
+        voice: <p>TODO: Voice</p>,
+        auth: <WidgetAuthScreen/>,
+        inbox: <p>TODO: Inbox</p>,
+        chat: <p>TODO: Chat</p>,
+        contact: <p>TODO: Contact</p>
+    }
+
     return (
         <main
             className="min-h-screen min-w-screen flex h-full w-full flex-col overflow-hidden rounded-xl border bg-muted"
         >
-            <WidgetAuthScreen/>
-            {/*<WidgetFooter/>*/}
+            {screenComponents[screen]}
         </main>
     )
 };
